d0426ae1b5869807e0b4e51ea091d8464f660ac0
Brian Duncan <briandduncan@comcast.net>
2017-08-24 18:22:53 +0000

    creating index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        
+        <h1>Brian Duncan's CST352</h1>
+        
+        
+        
+        
+        
+        
+
+    </body>
+</html>
