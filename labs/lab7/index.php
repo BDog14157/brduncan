c54447bfaea9a1d41474988b8723fa6826ce64f8
Brian Duncan <briandduncan@comcast.net>
2017-11-11 19:17:37 +0000

    adding lab 8

--- labs/lab7/index.php
+++ labs/lab7/index.php
@@ -1,3 +1,47 @@
+<?php
+session_start(); //starts or resumes an existing session
+//print_r($_POST); //displays values passed from login form
+//validates the username and password
+include '../../dbConnection.php';
+$conn = getDatabaseConnection();
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+//echo $password;
+//Following SQL works but it allows SQL Injection!!
+// $sql = "SELECT *
+//         FROM q_admin
+//         WHERE username = '$username' 
+//         AND   password = '$password'";
+        
+$sql = "SELECT *
+        FROM q_admin
+        WHERE username = :username 
+        AND   password = :password";   
+$namedParameters  = array();
+$namedParameters[':username']  = $username;
+$namedParameters[':password']  = $password;
+$stmt = $conn->prepare($sql);
+$stmt->execute($namedParameters);
+$record = $stmt->fetch(PDO::FETCH_ASSOC);
+//print_r($record);
+if (empty($record)) {
+    
+    if(isset($_POST['username'])){
+    
+  echo "Wrong credentials!";  
+    }
+} else {
+    
+    $_SESSION['username'] = $record['username'];
+    $_SESSION['adminFullName'] = $record['firstName'] . " " . $record['lastName'];
+   // echo $_SESSION['adminFullName'];
+   //echo "Successful login!";
+   header('Location: admin.php'); //redirects users to admin page
+   
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +52,7 @@
 
         <h1> Admin Login</h1>
         
-        <form method="POST" action="loginProcess.php">
+        <form method="POST">
     
         Username: <input type="text" name="username"/> <br />
         Password: <input type="password" name="password"/> <br />
--- labs/lab7/loginProcess.php
+++ labs/lab7/loginProcess.php
@@ -1,19 +1,12 @@
 <?php
 session_start(); //starts or resumes an existing session
-
-
 //print_r($_POST); //displays values passed from login form
-
 //validates the username and password
 include '../../dbConnection.php';
 $conn = getDatabaseConnection();
-
 $username = $_POST['username'];
 $password = sha1($_POST['password']);
-
 //echo $password;
-
-
 //Following SQL works but it allows SQL Injection!!
 // $sql = "SELECT *
 //         FROM q_admin
@@ -24,17 +17,13 @@ $sql = "SELECT *
         FROM q_admin
         WHERE username = :username 
         AND   password = :password";   
-
 $namedParameters  = array();
 $namedParameters[':username']  = $username;
 $namedParameters[':password']  = $password;
-
 $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
 $record = $stmt->fetch(PDO::FETCH_ASSOC);
-
 //print_r($record);
-
 if (empty($record)) {
     
   echo "Wrong credentials!";  
@@ -48,9 +37,4 @@ if (empty($record)) {
    header('Location: admin.php'); //redirects users to admin page
    
 }
-
-
-
-
-
 ?>
